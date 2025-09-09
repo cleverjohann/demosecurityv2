f5cb2fd7c48569e9ab5dc19ea9043cfecb7c3036
Clever Machaca <106214364+cleverjohann@users.noreply.github.com>
2025-09-08 19:39:37 -0500

    initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/main/java/com/platform/demosecurityv2/Demosecurityv2Application.java
@@ -0,0 +1,13 @@
+package com.platform.demosecurityv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demosecurityv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demosecurityv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/platform/demosecurityv2/Demosecurityv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.demosecurityv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosecurityv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
